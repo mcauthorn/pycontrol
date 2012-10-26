5af67298b64d04316f29970e1cabe8dd143e0ce1
Matt Cauthorn <matt@mattium.sea.i.extrahop.com>
2012-10-26 17:41:56 -0400

    rearranging for pypi.

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 from setuptools import setup
 
 setup(
@@ -8,9 +7,12 @@ setup(
                         with F5's BIG-IP iControl management API.""",
     version='2.1',
     license='GPL',
+    author='Matt Cauthorn',
+    author_email='mcauthorn@gmail.com',
     url='https://github.com/mcauthorn/pycontrol',
     keywords='iControl F5 API',
-    py_modules=['pycontrol'],
+    #py_modules=['pycontrol'],
+    packages=['pycontrol',],
     install_requires=['distribute', 'suds>=0.3.9'],
     platforms='any',
     classifiers=[
