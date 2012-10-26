ccf661cc16a41dc3961ae7f83b4378e127b1a9d3
Matt Cauthorn <matt@mattium.sea.i.extrahop.com>
2012-10-26 17:30:41 -0400

    Some final changes for setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup
+
+setup(
+    name='pycontrol',
+    description='Library for F5 iControl API',
+    long_description="""pyControl is a Python-based library that integrates
+                        with F5's BIG-IP iControl management API.""",
+    version='2.1',
+    license='GPL',
+    url='https://github.com/mcauthorn/pycontrol',
+    keywords='iControl F5 API',
+    py_modules=['pycontrol'],
+    install_requires=['distribute', 'suds>=0.3.9'],
+    platforms='any',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
